54a527d0cd131d2e9ec52c410763cd39b62eee7c
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-06 22:47:46 +0100

    ggh

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,6 +3,26 @@
 //
 #include "Factorial.h"
 
-int factorial(int value) {
-  return 0;
+int factorial(int value)
+{
+    int il=1;
+
+    if (value==0 || value==1)
+        return 1;
+    if (value+13<=0 || value>=13)
+        return 0;
+    else
+    {
+        if(value>0)
+        {
+            for (int i = 2; i <= value; i++)
+                il = il * i;
+        }
+        else
+        {
+            for(int i=value; i<0;i++)
+                il=il*i;
+        }
+        return il;
+    }
 }
