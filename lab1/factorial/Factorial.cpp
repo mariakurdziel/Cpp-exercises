b93f639a3a66703d9c830abbae03093578612fb7
mrozolga <m.olgaa97@gmail.com>
2017-03-07 18:34:22 +0100

    rozwiazanie factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,22 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+    if (value>=13)
+    {
+        return 0;
+    }
+    int fact = 1;
+    if (value > 0) {
+        for (int i = 1; i <= value; i++) {
+            fact = fact * i;
+        }
+    } else {
+        if (value == 0) fact = 1;
+        else {
+            for (int i = value; i < 0; i++) {
+                fact = fact * i;
+            }
+        }
+    }
+    return fact;
 }
