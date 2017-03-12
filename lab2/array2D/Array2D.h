95711b3a014b8b72b5d49ea3262b1754720d8bd7
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-12 22:46:53 +0100

    cc

---
+++ lab2/array2D/Array2D.h
@@ -0,0 +1,11 @@
+//
+// Created by maria on 11.03.17.
+//
+
+#ifndef JIMP_EXERCISES_ARRAY2D_H
+#define JIMP_EXERCISES_ARRAY2D_H
+int **Array2D(int n_rows,int n_cols);
+void DeleteArray2D(int **array, int n_rows, int n_columns);
+void FillArray2D(int **array, int n_rows, int n_columns);
+void PrintArray(int**array, int n_rows,int n_columns);
+#endif //JIMP_EXERCISES_ARRAY2D_H
