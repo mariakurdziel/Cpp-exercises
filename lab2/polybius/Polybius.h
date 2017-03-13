f84efc52cae647c1be9a3bcb2aa3da29bfbf5c4f
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-13 19:44:00 +0100

    cc

---
+++ lab2/polybius/Polybius.h
@@ -0,0 +1,15 @@
+//
+// Created by maria on 11.03.17.
+//
+
+#ifndef JIMP_EXERCISES_POLYBIUS_H
+#define JIMP_EXERCISES_POLYBIUS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string PolybiusCrypt(std::string message);
+std::string PolybiusDecrypt(std::string crypted);
+
+#endif //JIMP_EXERCISES_POLYBIUS_H
