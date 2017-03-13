621f4f5c450f57aa5cca65c0191a197db9f32c9b
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-13 20:07:07 +0100

    cc

--- lab2/array2D/main.cpp
+++ lab2/array2D/main.cpp
@@ -4,22 +4,15 @@
 
 #include <iostream>
 #include "Array2D.h"
-
-
 using namespace std;
 
-int main()
-{
-    int **tab;
-    int x,y;
-    cout<<"Podaj ilosc wierszy:"<<endl;
-    cin>>x;
-    cout<<"Podaj ilosc kolumn:"<<endl;
-    cin>>y;
-
-    tab=Array2D(x,y);
-    FillArray2D(tab,x,y);
-    PrintArray(tab,x,y);
-    DeleteArray2D(tab,x,y);
-    return 0;
+int main(void){
+    int a,b;
+    cout<<"Give me 2 numbers: "<<endl;
+    cin>>a;
+    cin>>b;
+    int **tab=NewArray2D(a, b);
+    PrintArray2D(tab, a, b);
+    DeleteArray2D(tab, a, b);
 }
+
