e9a4225a4718570486df1cab30a94bf5f5706c60
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-13 19:42:23 +0100

    cc

---
+++ lab2/greatestproduct/GreatestProduct.h
@@ -0,0 +1,14 @@
+//
+// Created by maria on 13.03.17.
+//
+
+#ifndef JIMP_EXERCISES_GREATESTPRODUCT_H
+#define JIMP_EXERCISES_GREATESTPRODUCT_H
+
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+int GreatestProduct(const std::vector<int> &numbers, int k);
+
+#endif //JIMP_EXERCISES_GREATESTPRODUCT_H
