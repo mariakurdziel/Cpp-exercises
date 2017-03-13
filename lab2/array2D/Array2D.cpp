77637f5925f45259f80bcc45ce1c15994e54be01
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-13 20:06:42 +0100

    cc

--- lab2/array2D/Array2D.cpp
+++ lab2/array2D/Array2D.cpp
@@ -1,53 +1,55 @@
 //
 // Created by maria on 09.03.17.
 //
-#include <Array2D.h>
-#include <iostream>
 
+#include "Array2D.h"
 using namespace std;
 
-void DeleteArray2D(int **array,int n_rows, int n_columns)
-{
-    for(int i=0; i<n_rows;i++)
-        delete [] array[i];
-    delete [] array;
-}
 
-void PrintArray(int**tab, int n_rows,int n_columns)
+void PrintArray2D (int** tab, int row, int col)
 {
-   for(int i=0; i<n_rows;i++)
-   {
-      for(int j = 0;j<n_columns;j++)
-      {
-          cout<<tab[i][j];
-          cout<<" ";
-      }
-   cout<<"\n";
-   }
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
+        {
+            std::cout<<tab[i][j]<<" ";
+            if (j==col-1)
+            {
+                cout<<endl;
+            }
+        }
+    }
 }
-void FillArray2D(int **tab, int n_rows, int n_columns)
+void FillArray2D (int row, int col, int** tab)
 {
-   int n=0;
-   for(int i=0; i<n_rows;i++)
-   {
-      for(int j = 0;j<n_columns;j++)
-         {
+    int n=1;
+    for (int i=0; i<row; i++)
+    {
+        for (int j=0; j<col; j++)
+        {
             tab[i][j]=n;
             n++;
-         }
-   }
+        }
+    }
 }
-
-int **Array2D(int n_rows, int n_columns)
+int** NewArray2D(int n_rows, int n_columns)
 {
-    if(n_rows<=0 || n_columns<=0)
-        return nullptr;
-    else
-    {
+    if (n_rows<=0 || n_columns<=0) return nullptr;
+    else {
         int **tab = new int *[n_rows];
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < n_rows; i++) {
             tab[i] = new int[n_columns];
+        }
+        FillArray2D(n_rows, n_columns, tab);
         return tab;
     }
 }
 
+void DeleteArray2D(int** array, int n_rows, int n_columns)
+{
+    for (int i=0; i<n_rows; i++)
+    {
+        delete [] array[i];
+    }
+    delete [] array;
+}
