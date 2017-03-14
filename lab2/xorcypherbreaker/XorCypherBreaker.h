9ecc8e2e90d023082fc370411e07e59ef50eb12e
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-14 16:48:57 +0100

    cc

---
+++ lab2/xorcypherbreaker/XorCypherBreaker.h
@@ -0,0 +1,14 @@
+//
+// Created by maria on 13.03.17.
+//
+
+#ifndef JIMP_EXERCISES_XORCYPHERBREAKER_H
+#define JIMP_EXERCISES_XORCYPHERBREAKER_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+std::string XorCypherBreaker(const std::vector<char> &cryptogram,int key_length, const std::vector<std::string> &dictionary);
+#endif //JIMP_EXERCISES_XORCYPHERBREAKER_H
