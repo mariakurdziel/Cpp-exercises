5509a4071fcf64c03260ac680c330443ab759f0f
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-16 21:03:55 +0100

    cc

--- lab2/xorcypherbreaker/XorCypherBreaker.cpp
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 string XorCypherBreaker(const vector<char> &cryptogram,int key_length, const vector <string> &dictionary) {
-    vector<int> key;
+    vector<char> key;
     string word;
     vector<string> message;
     int n = 0;
@@ -18,33 +18,32 @@ string XorCypherBreaker(const vector<char> &cryptogram,int key_length, const vec
 
 
     for (int i = 97; i <= 122; i++)
-    {
         for (int j = 97; j <= 122; j++)
-        {
             for (int k = 97; k <= 122; k++)
             {
-                key.emplace_back(i);
-                key.emplace_back(j);
-                key.emplace_back(k);
+                key.push_back(char(i));
+                key.push_back(char(j));
+                key.push_back(char(k));
 
 
                 for (int l = 0; l < cryptogram.size(); l++)
                 {
                     if (n < 3)
                     {
-                        if ((key[n]^(int)cryptogram[l])>= 97 && (key[n]^(int)cryptogram[l]) <= 122)
+                        if ((key[n] ^ cryptogram[l])>= 97 && (key[n] ^ cryptogram[l]) <= 122)
                         {
-                            x =char (key[n] ^ (int)cryptogram[l]);
+                            x =key[n] ^ cryptogram[l];
                             word += x;
                             n++;
                         }
-                        if ((key[n]^(int)cryptogram[l])>= 65 && (key[n]^(int)cryptogram[l]) <= 90)
+
+                        if ((key[n] ^ cryptogram[l])>= 65 && (key[n] ^ cryptogram[l]) <= 90)
                         {
-                            x =char (key[n] ^ (int)cryptogram[l]);
+                            x =key[n] ^ cryptogram[l];
                             word += x;
                             n++;
                         }
-                        else
+                       if((key[n] ^ cryptogram[l])<65 || (key[n] ^ cryptogram[l]) >90 && (key[n] ^ cryptogram[l])<97 || (key[n] ^ cryptogram[l])>122)
                         {
                             message.emplace_back(word);
                             word = "";
@@ -53,8 +52,13 @@ string XorCypherBreaker(const vector<char> &cryptogram,int key_length, const vec
                     }
                     if (n == 3)
                         n = 0;
-                }
 
+                    if(l==cryptogram.size()-1)
+                    {
+                        message.emplace_back(word);
+                        word = "";
+                    }
+                }
 
     for (int i = 0; i < dictionary.size(); i++)
     {
@@ -65,17 +69,15 @@ string XorCypherBreaker(const vector<char> &cryptogram,int key_length, const vec
      {
          max = m;
          decode = "";
-         for (int i = 0; i < key.size(); i++)
-             decode += char(key[i]);
+         for (int i =0; i<key.size(); i++)
+             decode +=key[i];
      }
 
     key.clear();
     message.clear();
-    word="";
     m=0;
     n=0;
   }
- }
-}
+
     return decode;
 }
