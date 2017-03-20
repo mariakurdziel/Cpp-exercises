38cf96760b307dfb91dfd60fc1ee666c8e233523
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-20 12:20:35 +0100

    xx

--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -10,10 +10,26 @@
 #include <array>
 #include <memory>
 #include <iostream>
+#include <map>
 
-struct TinyUrlCodec;
-std::unique_ptr<TinyUrlCodec> Init();
-void NextHash(std::array<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+using std::string;
+using std::map;
+using std::array;
+
+namespace tinyurl
+{
+    struct TinyUrlCodec
+    {
+        map<string, string> url_map={{"00000","00000"}};
+        array<char, 6> tab;
+    };
+
+    std::unique_ptr<TinyUrlCodec> Init();
+
+    void NextHash(std::array<char, 6> *state);
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+}
 #endif //JIMP_EXERCISES_TINYURL_H
