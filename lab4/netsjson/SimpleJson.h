7df2dd39f1169b7dc4f0ce8b8e4a61f621ba8533
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-28 10:15:26 +0200

    lab4

---
+++ lab4/netsjson/SimpleJson.h
@@ -0,0 +1,48 @@
+//
+// Created by maria on 27.03.17.
+//
+
+#ifndef JIMP_EXERCISES_SIMPLEJSON_H
+#define JIMP_EXERCISES_SIMPLEJSON_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <experimental/optional>
+#include <string>
+namespace nets
+{
+    using namespace std;
+
+    class JsonValue
+    {
+    public:
+        JsonValue(int numberc);
+        JsonValue(double numberz);
+        JsonValue(std::string word);
+        JsonValue(bool value);
+        JsonValue(vector <JsonValue> v);
+        JsonValue(map <string,JsonValue> obj);
+
+        ~JsonValue();
+
+        std::experimental::optional<JsonValue> ValueByName(const std::string &name) const;
+        std::string ToString() const;
+
+    private:
+        int number1;
+        double number2;
+        string word;
+        bool val;
+        vector <JsonValue> tab;
+        map <string,JsonValue> object;
+
+
+
+
+    };
+
+}
+
+#endif //JIMP_EXERCISES_SIMPLEJSON_H
