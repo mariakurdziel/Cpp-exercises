41b43ae4719a66f33cf943081a46719ca253fa6c
mariakurdziel <marysiazk@poczta.onet.pl>
2017-03-28 15:59:36 +0200

    lab4

--- lab4/netsjson/SimpleJson.h
+++ lab4/netsjson/SimpleJson.h
@@ -5,45 +5,38 @@
 #ifndef JIMP_EXERCISES_SIMPLEJSON_H
 #define JIMP_EXERCISES_SIMPLEJSON_H
 
+
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
 #include <experimental/optional>
-#include <string>
+#include <sstream>
 namespace nets
 {
     using namespace std;
 
     class JsonValue
     {
+        string obj;
     public:
-        JsonValue(int numberc);
-        JsonValue(double numberz);
-        JsonValue(std::string word);
-        JsonValue(bool value);
-        JsonValue(vector <JsonValue> v);
-        JsonValue(map <string,JsonValue> obj);
+        JsonValue(const int x);
+        JsonValue(const double x);
+        JsonValue(const bool x);
+        JsonValue(const std::string &name);
+        JsonValue(const std::vector<JsonValue> &array);
+        JsonValue(const std::map<std::string, JsonValue> &map1);
 
         ~JsonValue();
 
         std::experimental::optional<JsonValue> ValueByName(const std::string &name) const;
         std::string ToString() const;
 
-    private:
-        int number1;
-        double number2;
-        string word;
-        bool val;
-        vector <JsonValue> tab;
-        map <string,JsonValue> object;
-        char type;
-
-
-
-
+        void replace_substr(string &str, const string &pattern1, const string &pattern2) const;
     };
 
 }
 
 #endif //JIMP_EXERCISES_SIMPLEJSON_H
+
+
