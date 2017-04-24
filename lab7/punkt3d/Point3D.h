5e3bf753140e13d85aaef292634c5664c8a9753d
mariakurdziel <marysiazk@poczta.onet.pl>
2017-04-25 01:19:32 +0200

    Lab7

---
+++ lab7/punkt3d/Point3D.h
@@ -0,0 +1,44 @@
+//
+// Created by maria on 21.04.17.
+//
+
+#ifndef JIMP_EXERCISES_PUNKT3D_H
+#define JIMP_EXERCISES_PUNKT3D_H
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ostream>
+
+
+class Point {
+public:
+    Point();
+    Point(double x, double y);
+    ~Point();
+
+    void ToString(std::ostream *out) const;
+    double Distance(const Point &other) const;
+
+    double GetX() const;
+    double GetY() const;
+
+    void SetX(double x);
+    void SetY(double y);
+protected:
+    double x_, y_;
+};
+
+class Point3D : public Point
+{
+private:
+    double z_;
+public:
+    Point3D(double a, double b, double z);
+    double DoubleDistance(const Point3D &other) const;
+    double GetZ() const;
+    ~Point3D();
+};
+
+#endif //JIMP_EXERCISES_PUNKT3D_H
