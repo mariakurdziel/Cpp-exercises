f1003007ba2e641da868f4d0f834eba8b4fce084
mrozolga <EWOKszsz3/46>
2017-05-09 12:21:17 +0200

    lab8, MovieSubtitles, correct subrip

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -27,6 +27,8 @@ namespace moviesubs {
         while (regex_search(s, matches, pattern)) {
             time = stoi(matches[1].str());
             time += dif;
+            if (time<0)
+                throw NegativeFrameAfterShift(s, nrline);
             *output << "{" << time << "}";
 
             s = matches.suffix().str();
@@ -46,22 +48,30 @@ namespace moviesubs {
     }
 
     void SubRipSubtitles::Delay(int delay, int fps, istream *input, ostream *output, int nrline) {
-        int time[7], mili = 0, blok = 0, tekst = 0;
+        int time[7], mili = 0, blok = 1, oldmili = 0;
         regex pattern2{R"(\d+)"};
         regex pattern{R"((\d+):(\d+):(\d+),(\d+) --> (\d+):(\d+):(\d+),(\d+))"};
         smatch matches;
         string s;
         getline(*input, s);
 
+//        if (nrline>1) *output<<endl;
+        if (fps < 0)
+            throw invalid_argument("FPS cannot be negative");
         if (regex_match(s, pattern2)) {
             if (stoi(s) == nrline) {
                 *output << s << endl;
-                nrline++;
+                blok++;
                 getline(*input, s);
 
                 if (regex_search(s, matches, pattern)) {
+
+
                     for (int i = 0; i < 8; i++) time[i] = stoi(matches[i + 1].str());
+
+
                     mili = 1000 * time[2] + 60000 * time[1] + 3600000 * time[0] + time[3];
+                    oldmili = mili;
                     mili += delay;
                     time[3] = mili % 1000;
                     mili = mili / 1000;
@@ -73,6 +83,9 @@ namespace moviesubs {
 
                     mili = 1000 * time[6] + 60000 * time[5] + 3600000 * time[4] + time[7];
                     mili += delay;
+                    oldmili += delay;
+                    if (mili < oldmili)
+                        throw SubtitleEndBeforeStart(s, nrline);
                     time[7] = mili % 1000;
                     mili = mili / 1000;
                     time[6] = mili % 60;
@@ -80,7 +93,8 @@ namespace moviesubs {
                     time[5] = mili % 60;
                     mili = mili / 60;
                     time[4] = mili;
-                    blok++;
+
+
                     *output << setfill('0') << setw(2) << time[0] << ":" << setfill('0') << setw(2)
                             << time[1] << ":" << setfill('0') << setw(2) << time[2] << "," << setfill('0')
                             << setw(3)
@@ -89,20 +103,76 @@ namespace moviesubs {
                             << setfill('0')
                             << setw(3) << time[7] << endl;
 
+                }else {
+                    if (findchar(s, '.') == true)
+                        throw InvalidSubtitleLineFormat(s, nrline - 1);
+                    if (findchar(s, ';') == true)
+                        throw InvalidSubtitleLineFormat(s, nrline - 1);
+                    if (findchar(s, '--->') == true)
+                        throw InvalidSubtitleLineFormat(s, nrline - 1);
+                    if (occurences(s, "-->") != 1)
+                        throw InvalidSubtitleLineFormat(s, nrline - 1);
                 }
                 while (not s.empty()) {
                     getline(*input, s);
-                    *output << s;
-                    if (not s.empty()) *output<<endl;
+                    *output << s << endl;
 
                 }
+                *output;
 
 
-
+            } else throw OutOfOrderFrames(s, nrline-1);
+            if (nrline==2) {
+                if (delay < 0)
+                    throw NegativeFrameAfterShift(s, nrline - 1);
             }
         }
     }
 
 
-}
 
+
+    NegativeFrameAfterShift::NegativeFrameAfterShift(string str, int line) : SubtitlesValidationException
+        ("At line " + std::to_string(line) + ": " + str, line) {};
+
+
+    SubtitleEndBeforeStart::SubtitleEndBeforeStart(string str, int line) : SubtitlesValidationException
+        ("At line " + std::to_string(line) + ": " + str, line) {};
+
+
+    MissingTimeSpecification::MissingTimeSpecification(string str, int line) : SubtitlesValidationException
+        ("At line " + std::to_string(line) + ": " + str, line) {};
+
+
+    OutOfOrderFrames::OutOfOrderFrames(string str, int line) : SubtitlesValidationException
+        ("At line " + std::to_string(line) + ": " + str, line) {};
+
+
+
+
+    InvalidSubtitleLineFormat::InvalidSubtitleLineFormat(string str, int line) : SubtitlesValidationException
+         ("At line " + std::to_string(line) + ": " + str, line) {};
+
+    bool findchar( std::string & tekst, char szukanyZnak )
+    {
+        size_t znalezionaPozycja = tekst.find( szukanyZnak );
+        if( znalezionaPozycja == std::string::npos )
+            return false;
+        else
+            return true;
+
+    }
+
+    int occurences (string & tekst, string szukany )
+    {
+        int occurrences1 = 0;
+        string::size_type start = 0;
+
+        while ((start = tekst.find(szukany, start)) != string::npos) {
+            ++occurrences1;
+            start += szukany.length(); // see the note
+        }
+        return occurrences1;
+    }
+
+}
--- lab8/moviesubtitles/MovieSubtitles.h
+++ lab8/moviesubtitles/MovieSubtitles.h
@@ -12,6 +12,8 @@
 #include <regex>
 #include <sstream>
 #include <memory>
+#include <stdexcept>
+#include <algorithm>
 
 using std::ostream;
 using std::istream;
@@ -38,7 +40,54 @@ namespace moviesubs
         virtual void Delay (int delay, int fps, istream *input, ostream *output, int nrline);
     };
     string inputtostring(istream &input);
-}
+
+
+
+
+    class SubtitlesValidationException : public std::runtime_error
+    {
+    protected:
+        int line;
+
+    public:
+        virtual int LineAt() const { return line; }
+        SubtitlesValidationException(std::string str, int line) : line(line),  std::runtime_error(str) {}
+    };
+
+    class NegativeFrameAfterShift : public SubtitlesValidationException
+    {
+    public:
+        NegativeFrameAfterShift(string str, int line);
+    };
+
+    class SubtitleEndBeforeStart : public SubtitlesValidationException
+    {
+    public:
+        SubtitleEndBeforeStart(string str, int line);
+    };
+
+    class InvalidSubtitleLineFormat : public SubtitlesValidationException
+    {
+    public:
+        InvalidSubtitleLineFormat(string str, int line);
+    };
+
+    class MissingTimeSpecification : public SubtitlesValidationException
+    {
+    public:
+        MissingTimeSpecification(string str, int line);
+    };
+
+    class OutOfOrderFrames : public SubtitlesValidationException
+    {
+    public:
+        OutOfOrderFrames(string str, int line);
+    };
+
+    bool findchar( std::string & tekst, char szukanyZnak );
+    int occurences (string &tekst, string szukany );
+};
+
 
 
 #endif //JIMP_EXERCISES_MOVIESUBTITLES_H
