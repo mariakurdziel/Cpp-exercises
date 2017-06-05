e4b661cb1ed682613c60fec5ebfbf2c5339c09d5
mariakurdziel <marysiazk@poczta.onet.pl>
2017-06-05 20:17:55 +0200

    Przesyłamy rozwiązania do lab 12

---
+++ lab12/academiasequentialgenerator/SequentialGenerator.h
@@ -0,0 +1,13 @@
+//
+// Created by maria on 05.06.17.
+//
+
+#ifndef JIMP_EXERCISES_SEQUENTIALGENERATOR_H
+#define JIMP_EXERCISES_SEQUENTIALGENERATOR_H
+
+#include <iostream>
+
+using namespace std;
+
+
+#endif //JIMP_EXERCISES_SEQUENTIALGENERATOR_H
---
+++ lab12/academiasequentialgenerator/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by maria on 05.06.17.
+//
+
+#include "SequentialGenerator.h"
+
+int main()
+{
+    return 0;
+}
