1c55f8c30138bd71ca64b621ce3f687516fa541c
mariakurdziel <marysiazk@poczta.onet.pl>
2017-06-06 10:23:13 +0200

    Przesyłamy rozwiązania do lab 12

--- lab12/academiasequentialgenerator/SequentialGenerator.h
+++ lab12/academiasequentialgenerator/SequentialGenerator.h
@@ -6,8 +6,35 @@
 #define JIMP_EXERCISES_SEQUENTIALGENERATOR_H
 
 #include <iostream>
-
 using namespace std;
+
+template<class Id, class Y>
+class SequentialIdGenerator
+{
+    public:
+
+        SequentialIdGenerator(const Y &counter =Y()) {
+           counter_=counter;
+        }
+        ~SequentialIdGenerator(){}
+        Id NextValue();
+
+
+    private:
+        Y counter_;
+
+};
+
+template<class Id, class Y>
+Id SequentialIdGenerator<Id,Y>::NextValue()
+{
+    Id new_=Id(counter_);
+    ++counter_;
+    return new_;
+
+}
+
+
 
 
 #endif //JIMP_EXERCISES_SEQUENTIALGENERATOR_H
